06429d071554a6364b0d93b30de4dab3bda6e9ca
ubeaut <ubeaut58@gmail.com>
2021-04-24 19:05:05 +1000

    Update get-icons.sh
    Changed curl so it works as BOM has stopped web scaping

--- get-icons.sh
+++ get-icons.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #This grabs the icons that can be used in OH3
-//wget http://www.bom.gov.au/images/symbols/large/sunny.png
 
 cd /etc/openhab/icons/classic/
 
 for ICON in storm sunny clear cloudy hazy light-rain wind fog showers rain dust frost snow light-showers heavy-showers partly-cloudy light-rain 
 do
-sudo wget -N http://www.bom.gov.au/images/symbols/large/$ICON.png
+sudo curl http://www.bom.gov.au/images/symbols/large/$ICON.png -H 'Connection: keep-alive'   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'   -H 'Accept: image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8'   -H 'Referer: http://www.bom.gov.au/products/IDR033.shtml'   -H 'Accept-Language: en-AU,en-GB;q=0.9,en-US;q=0.8,en;q=0.7' --output $ICON.png
 #read crap
 done
 
